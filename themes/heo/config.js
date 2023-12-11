d967ea6810a09753e55b18328320c3666e5100e3
bai ying <cn.baiying@gmail.com>
2023-12-11 18:07:00 +0800

    update

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: 'Welcome to my blog', url: 'https://www.baiying.io' },
+    { title: 'Welcome to my blog', url: 'https://www.baiying.io' }
   ],
 
   // 英雄区(首页顶部大卡)
